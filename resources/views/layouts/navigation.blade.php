1d79d246a14ac8a7b105a6ebe3f3ba6b2badefad
RenanWth <renanalex2003@hotmail.com>
2025-07-07 15:29:27 -0300

    alteracoes navegacao telas

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -5,19 +5,19 @@
             <div class="flex">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
-                    <a href="{{ route('produtos.index') }}">
+                    <a href="{{ route('tarefas.index') }}">
                         <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
                     </a>
                 </div>
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('produtos.index')" :active="request()->routeIs('produtos.*')">
-                        {{ __('Produtos') }}
+                    <x-nav-link :href="route('tarefas.index')" :active="request()->routeIs('tarefas.*')">
+                        {{ __('Tarefas') }}
                     </x-nav-link>
                 </div>
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('produtos.index')" :active="request()->routeIs('emails.*')">
+                    <x-nav-link :href="route('tarefas.index')" :active="request()->routeIs('emails.*')">
                         {{ __('Email') }}
                     </x-nav-link>
                 </div>
@@ -72,8 +72,8 @@
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link :href="route('produtos.index')" :active="request()->routeIs('produtos.*')">
-                {{ __('Produtos') }}
+            <x-responsive-nav-link :href="route('tarefas.index')" :active="request()->routeIs('tarefas.*')">
+                {{ __('Tarefas') }}
             </x-responsive-nav-link>
         </div>
 
